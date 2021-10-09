d70f284320798147c4bcbf58476ca40dd652af23
Domingo Pelago <domingo.pelago@coreproc.ph>
2021-10-09 09:56:51 +0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <title>{{ config('app.name') }}</title>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script src="{{ mix('/js/app.js') }}" defer></script>
+</head>
+<body>
+<div id="app">
+
+</div>
+</body>
+</html>
