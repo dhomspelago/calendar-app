be8591bae846af8330c7d94570b34aeba4ed04f2
Domingo Pelago <domingo.pelago@coreproc.ph>
2021-10-10 10:03:28 +0800

    fix https issue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
   <title>{{ config('app.name') }}</title>
   <meta charset="UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
